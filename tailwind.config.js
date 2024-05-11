360b2c4dd0d07be701607b8362ea18a3c181c1d0
dangen-effy <dangen.effy@gmail.com>
2024-05-11 16:07:46 +0900

    feat: 기본 폰트 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
+		fontFamily: {
+			sans: ['GowunDodum', 'ui-sans-serif', 'system-ui']
+		},
 		extend: {
 			colors: {
 				primary: {
